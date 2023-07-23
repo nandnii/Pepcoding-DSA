7712fb5b00ce42948a6dc21d2b02031fe0c170fc
nandnii <nandnican@gmail.com>
2023-07-23 17:05:58 +0530

    Get Keypad Combination

--- 03. Recursion/KeypadCombinations.java
+++ 03. Recursion/KeypadCombinations.java
@@ -11,7 +11,7 @@ public class KeypadCombinations {
         scn.close();
     }
 
-    // input is 789
+    // input is 789         {0,     1,     2,     3,     4,     5,     6,      7,    8,     9  }
     static String[] codes = {".,", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tu", "vwx", "yz"};
     
     public static ArrayList<String> getKPC(String str) {
@@ -21,19 +21,24 @@ public class KeypadCombinations {
             return BaseAns;
         }
 
-        char ch = str.charAt(0);  // character 7
+        char ch = str.charAt(0);             // character 7
         String ros = str.substring(1);  // 89
-        
-        ArrayList<String> tempAns = getKPC(ros);  // 6 combination words of 89 (3*2)
+
+        ArrayList<String> tempAns = getKPC(ros);   // 6 combination words of 89 (3*2)
         
         ArrayList<String> finalAns = new ArrayList<>(); // should give 12 words (2*6)
 
-        // Right now ch is '6' which inform of integer is 54, so we have to subract '0'/48 to convert it to 6 integer
-        String code_for_ch = codes[ch - '0']; // "tu"
+        // Right now ch is '7' which inform of integer is 54
+        // so we have to subract '0'/48 to convert it to 7 integer
+        String code_for_ch = codes[ch - '0'];       // "tu"
 
-        for(int i=0; i<code_for_ch.length(); i++){  // length is 2
-            for(String recursion_string: tempAns){
-                finalAns.add(code_for_ch.charAt(i) + recursion_string);
+        for(int i=0; i<code_for_ch.length(); i++){  // length is 2 
+
+            // code_for_ch.charAt(i) will pick ith letter of that code, so first "t" then "u"
+            // and add it to each element of tempAns
+
+            for(String st: tempAns){
+                finalAns.add(code_for_ch.charAt(i) + st);
             }
         }
         return finalAns;
