5839ea9ebc2b2e9f9c25624bb62922de02c125cc
nandnii <nandnican@gmail.com>
2023-07-23 15:44:07 +0530

    Display Array elements

--- 03. Recursion/DisplayArrayElements.java
+++ 03. Recursion/DisplayArrayElements.java
@@ -5,10 +5,12 @@ public class DisplayArrayElements {
     Scanner scn = new Scanner(System.in);
     int n = scn.nextInt();
     
+    // input in array
     int[] arr = new int[n]; 
     for(int i=0; i<n; i++){
       arr[i] = scn.nextInt();
     }
+
     DisplayArr(arr, 0);
 
     scn.close();
@@ -16,7 +18,8 @@ public class DisplayArrayElements {
 
   public static void DisplayArr(int[] arr, int idx) {
     if(idx >= arr.length) return;
-    System.out.println(arr[idx]);
-    DisplayArr(arr, idx+1);
+
+    System.out.println(arr[idx]); // print first element
+    DisplayArr(arr, idx+1);       // Consquent elements will be printed by recursion
   }
 }
