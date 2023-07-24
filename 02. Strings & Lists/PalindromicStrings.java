b286cd9942ef4b727bf70320f33d96d16923364e
nandnii <nandnican@gmail.com>
2023-07-24 15:21:22 +0530

    Print all palindromic substrings

--- 02. Strings & Lists/PalindromicStrings.java
+++ 02. Strings & Lists/PalindromicStrings.java
@@ -20,23 +20,22 @@ public class PalindromicStrings {
         }
         return true;
     }
-
-	public static void solution(String str){
+    
+    public static void solution(String str){
         for(int i=0; i<str.length(); i++){
-			for(int j=i+1; j<=str.length(); j++){
+            for(int j=i+1; j<=str.length(); j++){
                 String ss = str.substring(i,j);
-				if(isPalindrome(ss) == true){
+                if(isPalindrome(ss) == true){
                     System.out.println(ss);
                 }
-			}
-		}
-	}
-
-	public static void main(String[] args) {
-		Scanner scn = new Scanner(System.in);
-		String str = scn.next();
-		solution(str);
-
+            }
+        }
+    }
+    
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        String str = scn.next();
+        solution(str);
         scn.close();
 	}
 
