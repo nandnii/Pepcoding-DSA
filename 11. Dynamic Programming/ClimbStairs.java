6bd1075f0fa38085afc954776422a721590888b4
nandnii <nandnican@gmail.com>
2023-08-01 22:48:11 +0530

    Climb Stairs using Memoization & Tabulation

--- 11. Dynamic Programming/ClimbStairs.java
+++ 11. Dynamic Programming/ClimbStairs.java
@@ -5,13 +5,15 @@ public class ClimbStairs {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
         int[] qb = new int[n + 1];
-        int paths = countPaths(n, qb);
-        System.out.println(paths);
+        int pathsMemo = countPathsMemo(n, qb);
+        int pathsTab = countPathsTab(n);
+        System.out.println(pathsMemo);
+        System.out.println(pathsTab);
 
         scn.close();
     }
 
-    public static int countPaths(int n, int[] qb) {
+    public static int countPathsMemo(int n, int[] qb) {
         if (n == 0) {
             return 1;
         } else if (n < 0) {
@@ -20,11 +22,32 @@ public class ClimbStairs {
 
         if (qb[n] != 0) return qb[n];
 
-        int p1 = countPaths(n - 1, qb);
-        int p2 = countPaths(n - 2, qb);
-        int p3 = countPaths(n - 3, qb);
+        int p1 = countPathsMemo(n - 1, qb);
+        int p2 = countPathsMemo(n - 2, qb);
+        int p3 = countPathsMemo(n - 3, qb);
 
         qb[n] = p1 + p2 + p3;
         return p1 + p2 + p3;
+    }
+
+    public static int countPathsTab(int n){
+
+        // make array of n+1 size
+        int[] dp = new int[n+1];
+
+        dp[0] = 1;      // ways to go from step 0 to step 0 is 1, i.e. stand there
+        for(int i = 1; i <= n; i++){
+            if(i==1){
+                dp[i] = dp[i-1];
+            }
+            else if(i==2){
+                dp[i] = dp[i-1] + dp[i-2];
+            }
+            else {
+                dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+            }
+        }
+
+        return dp[n];
     }
 }
