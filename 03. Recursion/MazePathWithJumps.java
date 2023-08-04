5c1aec0065bb417728fad97a4a3e2735640f7b8d
nandnii <nandnican@gmail.com>
2023-08-04 17:21:14 +0530

    Get Maze Paths With Jumps

--- 03. Recursion/MazePathWithJumps.java
+++ 03. Recursion/MazePathWithJumps.java
@@ -7,7 +7,7 @@ public class MazePathWithJumps {
     int n = scn.nextInt();
     int m = scn.nextInt();
 
-    ArrayList< String> paths = getMazePaths(1, 1, n, m);
+    ArrayList< String> paths = getMazePathsWithJumps(1, 1, n, m);
     System.out.println(paths);
 
     scn.close();
@@ -17,37 +17,42 @@ public class MazePathWithJumps {
   // sc - source column
   // dr - destination row
   // dc - destination column
-  public static ArrayList< String> getMazePaths(int sr, int sc, int dr, int dc) {
+  public static ArrayList< String> getMazePathsWithJumps(int sr, int sc, int dr, int dc) {
 
+    // Return blank AL, means [.]
     if (sr == dr && sc == dc){
         ArrayList<String> BaseAns = new ArrayList<>();
         BaseAns.add("");
         return BaseAns;
     }
+
+    // Return empty AL, means [], nothing will add in this when we do paths.add on this
     else if (sr > dr || sc > dc){
         ArrayList<String> BaseAns = new ArrayList<>();
         return BaseAns;
     }
 
-    ArrayList< String> paths = new ArrayList< >();
+    ArrayList< String> paths = new ArrayList<>();
 
     // horizontal moves
-    for (int hms = 1; hms <= dc - sc; hms++) {
-      ArrayList< String> hpaths = getMazePaths(sr, sc + hms, dr, dc);
+    for (int hms = 1; hms <= dc - sc; hms++) {          // max horizontal we can move as gap of dc & sc; smallest jump of h1, max of dc-sc
+      ArrayList< String> hpaths = getMazePathsWithJumps(sr, sc + hms, dr, dc);
       for (String hpath : hpaths) {
         paths.add("h" + hms + hpath);
       }
     }
 
-    for (int vms = 1; vms <= dr - sr; vms++) {
-      ArrayList< String> vpaths = getMazePaths(sr + vms, sc, dr, dc);
+    // vertical moves
+    for (int vms = 1; vms <= dr - sr; vms++) {        // max vertical we can move as gap of dr & sr; smallest jump of v1, max of dr-sr
+      ArrayList< String> vpaths = getMazePathsWithJumps(sr + vms, sc, dr, dc);
       for (String vpath : vpaths) {
         paths.add("v" + vms + vpath);
       }
     }
 
+    // diagonal moves
     for (int dms = 1; dms <= dr - sr && dms <= dc - sc; dms++) {
-      ArrayList< String> dpaths = getMazePaths(sr + dms, sc + dms, dr, dc);
+      ArrayList< String> dpaths = getMazePathsWithJumps(sr + dms, sc + dms, dr, dc);
       for (String dpath : dpaths) {
         paths.add("d" + dms + dpath);
       }
