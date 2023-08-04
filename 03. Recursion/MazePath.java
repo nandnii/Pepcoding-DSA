f4709fdab33e7efd2d597989c28acf947172ab54
nandnii <nandnican@gmail.com>
2023-08-04 16:45:00 +0530

    Get Maze Paths

--- 03. Recursion/MazePath.java
+++ 03. Recursion/MazePath.java
@@ -16,34 +16,38 @@ public class MazePath {
     // dr - destination row
     // dc - destination column
     public static ArrayList<String> getMazePaths(int sr, int sc, int dr, int dc) {
-        if (sr == dr && sc == dc){
-            ArrayList<String> BaseAns = new ArrayList<>();
-            BaseAns.add("");
-            return BaseAns;
+
+        // Return blank AL, means [.]
+        if(sr==dr && sc==dc){
+            ArrayList<String> base = new ArrayList<String>();
+            base.add("");
+            return base;
         }
-
-        ArrayList<String> hpaths = new ArrayList< >();
-        ArrayList<String> vpaths = new ArrayList< >();
-
-        if(sc < dc){
-         hpaths = getMazePaths(sr, sc+1, dr, dc);
+        
+        ArrayList<String> Hpath = new ArrayList<String>();
+        ArrayList<String> Vpath = new ArrayList<String>();
+        
+        // To reach from (1,1) to (3,3), we assume we can reach from (1,1) to (2,3) by recursion
+        if(sr<dr){
+            Hpath = getMazePaths(sr, sc, dr-1, dc);
         }
-
-        if(sr < dr){
-            vpaths = getMazePaths(sr+1, sc, dr, dc);
+        
+        // To reach from (1,1) to (3,3), we assume we can reach from (1,1) to (3,2) by recursion
+        if(sc<dc){
+            Vpath = getMazePaths(sr, sc, dr, dc-1);
         }
-
-        ArrayList<String> paths = new ArrayList< >();
-
-        for(String hpath: hpaths){
-            paths.add("h" + hpath);
+        
+        ArrayList<String> finalPath = new ArrayList<String>();
+        
+        for(String path:Vpath){
+            finalPath.add(path+"v");
         }
-
-        for(String vpath: vpaths){
-            paths.add("v" + vpath);
+        
+        for(String path:Hpath){
+            finalPath.add(path+"h");
         }
-
-        return paths;
+        
+        return finalPath;
     }
 
 }
