17043f7146927b2334a4dff06e853ab7c25671e2
nandnii <nandnican@gmail.com>
2023-08-08 20:17:45 +0530

    Coin Combination to pay amount

--- 11. Dynamic Programming/CoinChangeCombination.java
+++ 11. Dynamic Programming/CoinChangeCombination.java
@@ -6,26 +6,25 @@ public class CoinChangeCombination {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
 
-        int[] arr = new int[n];
+        int[] coins = new int[n];
         for(int i=0; i<n; i++){
-            arr[i] = scn.nextInt();
+            coins[i] = scn.nextInt();
         }
 
-        int amt = scn.nextInt();
+        int target = scn.nextInt();
 
         scn.close();
 
-        // Every index of this array means How many ways to pay [i] rs by coins available, so answer will be dp[amt]
-        int[] dp = new int [amt+1];
+        // Every index of this array means How many ways to pay [i] rs by coins available, so answer will be dp[target]
+        int[] dp = new int [target+1];
         dp[0] = 1;                                  // one way to pay 0 that is don't pay anything
-    
-        for(int i=0; i<arr.length; i++){            // run outer loop same number of times as the number of coins (iterations)
-            int coinValue = arr[i];
-            for(int j=coinValue; j<dp.length; j++){     // run from coin number index to end of dp length
-                dp[j] = dp[j] + dp[j - coinValue];      // j - coinValue is basically the complement
+
+        for(int coin: coins){                      // run outer loop same number of times as the number of coins (iterations)
+            for(int j=coin; j <= target; j++){      // run from coin number index to end of dp length (min coin can pay is its value & max is target)
+                dp[j] = dp[j] + dp[j - coin];       // j - coinValue is basically the complement
             }
         }
 
-        System.out.println(dp[amt]);
+        System.out.println(dp[target]);
     }
 }
